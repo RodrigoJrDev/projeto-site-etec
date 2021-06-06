c95c6dda2f56f2ab86eb82a710a300e4b2531071
Rodrigo Junior <rodrigojunior0323@gmail.com>
2021-06-06 20:54:38 -0300

    Estrutura do site.

---
+++ template/javascript/main.js
@@ -0,0 +1,6 @@
+// Script de Scroll do Navbar
+
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
